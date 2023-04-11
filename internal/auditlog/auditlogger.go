601480aeae77132aee77d084bb2d567600fedc2f
battagel <matthew@battagel.me.uk>
2023-04-11 16:46:35 +0100

    Added audit logger using postgresql

---
+++ internal/auditlog/auditlogger.go
@@ -0,0 +1,32 @@
+package auditlog
+
+import (
+	"aegis/pkg/logger"
+)
+
+type Database interface {
+	CreateDatabase() error
+	CreateTable(string) error
+	Insert(string, string, string, string, string, string) error
+}
+
+type AuditLogger struct {
+	logger    logger.Logger
+	db        Database
+	tableName string
+}
+
+func CreateAuditLogger(logger logger.Logger, db Database, tableName string) (*AuditLogger, error) {
+	// Check that db and table exists
+	db.CreateTable(tableName)
+	return &AuditLogger{
+		logger:    logger,
+		db:        db,
+		tableName: tableName,
+	}, nil
+}
+
+func (a *AuditLogger) Log(bucketName, objectKey, result, antivirus, timestamp string) {
+	a.logger.Debugln("Adding audit log")
+	a.db.Insert(a.tableName, bucketName, objectKey, result, antivirus, timestamp)
+}
